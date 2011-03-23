386e5c2a7c6c439d26949892f7dada28f8167b89
Amir Hadzic <amir.hadzic@randomshouting.com>
2011-03-23 22:18:42 +0100

    Added the GPL Licence and a README file.

--- Fitgy/Point.h
+++ Fitgy/Point.h
@@ -1,3 +1,22 @@
+/**
+*    This file is part of FitGamePack.
+*
+*    FitGamePack is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation, either version 3 of the License, or
+*    (at your option) any later version.
+*
+*    FitGamePack is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+*
+*    You should have received a copy of the GNU General Public License
+*    along with FitGamePack.  If not, see <http://www.gnu.org/licenses/>.
+*
+*    Author: Amir Hadzic <amir.hadzic@randomshouting.com>
+*/
+
 #ifndef FITGY_POINT_H
 #define FITGY_POINT_H
 
---
+++ README.markdown
@@ -0,0 +1,6 @@
+FitGamePack is composed of Fitgy, a simple 2D game engine and a few sample
+games.
+
+Please go to the
+[Wiki](http://redmine.randomshouting.com/projects/fitgamepack/wiki/Wiki) to
+learn more about the project and how to compile it.
