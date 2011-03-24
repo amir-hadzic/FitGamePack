80fd8606d8f43f06adf1c881e31a39d5f96bc657
Amir Hadzic <amir.hadzic@randomshouting.com>
2011-03-24 17:47:49 +0100

    Documented the Application class.

--- Fitgy/Application.h
+++ Fitgy/Application.h
@@ -32,6 +32,10 @@ namespace Fitgy {
     typedef std::vector<Entity*> EntityContainer;
     typedef EntityContainer::iterator EntityIterator;
 
+    /**
+     * This is the base class for a Fitgy Application. It handles the entities,
+     * event broadcasting and everything else required to run a game.
+     */
     class Application : public EventHandler {
     private:
         bool mRunning;
@@ -42,17 +46,76 @@ namespace Fitgy {
         void startRender();
 
     public:
+        /**
+         * Constructor.
+         */
         Application();
+
+        /**
+         * Destructor.
+         */
         ~Application();
+
+        /**
+         * Runs the Application loop.
+         *
+         * @return 0 if the application executed succesfully and anything else
+         * if it fails.
+         */
         int execute();
+
+        /**
+         * Returns the display entity.
+         *
+         * @return the display entity associated with this application.
+         */
         Display* getDisplay();
+
+        /**
+         * Adds an entity.
+         *
+         * @param entity is the target entity.
+         */
         void addEntity(Entity* entity);
+
+        /**
+         * Removes the entity.
+         *
+         * @param entity is the target entity.
+         */
         void removeEntity(Entity* entity);
 
+        /**
+         * Initializes the Application.
+         *
+         * Initializes the SDL libraries and creates an display entity.
+         */
         virtual bool init();
+
+        /**
+         * This method is called on each application loop iteration and this
+         * is where you should implement your game logic.
+         */
         virtual void loop();
+
+        /**
+         * This method is called on each application loop iteration and this is
+         * where you should implement your rendering code. By default this
+         * method calls the onRender() method of each entity.
+         */
         virtual void render();
+
+        /**
+         * This method is responsible for releasing any allocated resources such
+         * as entities, fonts, etc.
+         */
         virtual void cleanup();
+
+        /**
+         * onExit() implementation.
+         * return true if the event is handled and shouldn't be broadcasted
+         * further.
+         */
         virtual bool onExit();
     };
 }
